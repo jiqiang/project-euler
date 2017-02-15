cd91a1be42f73c3f3efd541afc43df4655fe9e8f
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-02-16 10:59:23 +1100

    add problem 7 solution

---
+++ p7/main.go
@@ -0,0 +1,16 @@
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+package main
+
+import (
+  "fmt"
+  "github.com/jiqiang/project-euler/p7/lib"
+)
+
+func main() {
+  fmt.Println(lib.P7(10001))
+}
