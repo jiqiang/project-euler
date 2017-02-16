2ed40cbe1481a188f581788f603a9c375f9791cc
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-02-16 14:08:09 +1100

    add problem 8 solution

---
+++ p8/lib/lib.go
@@ -0,0 +1,48 @@
+package lib
+
+import (
+  "strconv"
+  "strings"
+)
+
+func P8(s string) int64 {
+
+  numOfDigits := 13
+
+  var strs []string
+
+  var tempStr string
+
+
+  for i := 0; i < len(s); i++ {
+
+    tempStr = s[i:i + numOfDigits]
+
+    if !strings.Contains(tempStr, "0") {
+      strs = append(strs, tempStr)
+    }
+
+    if i + numOfDigits == len(s) {
+      break
+    }
+  }
+
+  var max, p int64 = 0, 1
+
+  for _, str := range strs {
+
+    p = 1
+
+    for _, r := range str {
+      d, _ := strconv.ParseInt(string(r), 10, 64)
+
+      p *= d
+    }
+
+    if max < p {
+      max = p
+    }
+  }
+
+  return max
+}
