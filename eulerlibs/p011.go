abdc5ce6d0bbb42f6ce3e6aab78849b4079210f4
Glenn Ji <qiang.ji@yahoo.com.au>
2017-02-20 20:36:16 +1100

    vertically check products

--- eulerlibs/p011.go
+++ eulerlibs/p011.go
@@ -17,3 +17,21 @@ func HorizontalCheck(grid [20][20]int64) int64 {
 	}
 	return max
 }
+
+// VerticalCheck retuns product of 4 adjcent numbers vertically.
+func VerticalCheck(grid [20][20]int64) int64 {
+	var p, max int64
+	max = 0
+	for i := 0; i+3 < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == 0 || grid[i+1][j] == 0 || grid[i+2][j] == 0 || grid[i+3][j] == 0 {
+				continue
+			}
+			p = grid[i][j] * grid[i+1][j] * grid[i+2][j] * grid[i+3][j]
+			if max < p {
+				max = p
+			}
+		}
+	}
+	return max
+}
