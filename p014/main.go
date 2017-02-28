4bcb45544f0bd8d7ee82682d738b25a1b43925a7
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-02-28 14:00:55 +1100

    update problem 14 solution

--- p014/main.go
+++ p014/main.go
@@ -3,32 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	var seqs [][]int
 	m := make(map[int]int)
-
-	for n := 2; n <= 13; n++ {
+	mc := 0
+	var lsn int
+	for n := 2; n <= 1000000; n++ {
 		var seq []int
 		sn := n
-		count := 1
 		seq = append(seq, n)
+		count := 1
 		for {
-
 			if sn == 1 {
 				break
 			}
-
 			if sn%2 == 0 {
 				sn = sn / 2
 			} else {
 				sn = sn*3 + 1
 			}
-
+			if c, ok := m[sn]; ok {
+				count += c
+				break
+			}
 			seq = append(seq, sn)
 			count++
 		}
-		seqs = append(seqs, seq)
-		m[n] = count
+		if mc < count {
+			mc = count
+			lsn = n
+		}
+		for idx, v := range seq {
+			_, ok := m[v]
+			if !ok {
+				m[v] = count - idx
+			}
+		}
 	}
-	fmt.Println(seqs)
-	fmt.Println(m)
+	fmt.Println(lsn)
 }
