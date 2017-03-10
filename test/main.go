ca9d4c703db57997f905ab0d4a410656db943d58
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-03-10 17:10:48 +1100

    update test

--- test/main.go
+++ test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type node struct {
@@ -9,20 +10,31 @@ type node struct {
 	y int
 }
 
-func display(n node, c chan node) {
+func process(in chan *node, out chan *node) {
 	go func() {
-		c <- n
+		for {
+			n := <-in
+			fmt.Println(*n)
+			if n.x < 2 {
+				out <- &node{n.x + 1, n.y}
+			}
+
+			if n.y < 2 {
+				out <- &node{n.x, n.y + 1}
+			}
+		}
 	}()
 }
 
 func main() {
 
-	n := node{0, 0}
+	in, out := make(chan *node), make(chan *node)
 
-	c := make(chan node)
+	process(in, out)
+	process(out, in)
 
-	display(n, c)
+	in <- &node{0, 0}
 
-	fmt.Println(<-c)
+	time.Sleep(3 * time.Second)
 
 }
