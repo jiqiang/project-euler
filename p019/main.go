6b267db5b953d0113314fc0a8ffc26ea542a4320
Qiang (Glenn) Ji <qiang.ji@ge.com>
2017-08-07 13:49:04 +1000

    add concurrent calculation

--- p019/main.go
+++ p019/main.go
@@ -3,17 +3,28 @@ package main
 import "fmt"
 import "time"
 
+func calculateInYear(year int, sundays chan int) {
+	sum := 0
+	for m := 1; m <= 12; m++ {
+		t := time.Date(year, time.Month(m), 1, 0, 0, 0, 0, time.UTC)
+		if t.Weekday().String() == "Sunday" {
+			sum++
+		}
+	}
+	sundays <- sum
+}
+
 func main() {
 
 	numOfSundays := 0
+	sundaysChan := make(chan int)
 
 	for y := 1901; y <= 2000; y++ {
-		for m := 1; m <= 12; m++ {
-			t := time.Date(y, time.Month(m), 1, 1, 0, 0, 0, time.UTC)
-			if t.Weekday().String() == "Sunday" {
-				numOfSundays++
-			}
-		}
+		go calculateInYear(y, sundaysChan)
+	}
+
+	for y := 1901; y <= 2000; y++ {
+		numOfSundays = numOfSundays + <-sundaysChan
 	}
 
 	fmt.Println(numOfSundays)
